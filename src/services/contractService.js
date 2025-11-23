20bb8ede32f19c0a0c9714480897c90d7b1a6d5c
samet <142526344+tebberen@users.noreply.github.com>
2025-11-23 03:23:15 +0300

    Handle Self gate check via read call

--- src/services/contractService.js
+++ src/services/contractService.js
@@ -20,7 +20,7 @@ import {
   SELF_GATE_ADDRESS,
   SELF_GATE_ABI,
 } from "../utils/constants.js";
-import { getWalletDetails } from "./walletService.js";
+import { checkCurrentNetwork, getWalletDetails } from "./walletService.js";
 import { sendWithReferral } from "./divviReferral.js";
 
 const READ_RPC_TIMEOUT = 20000;
@@ -288,15 +288,29 @@ export function getSelfGate(withWrite = false) {
 }
 
 export async function checkSelfVerification() {
-  const { address } = getWalletDetails();
+  const { address, provider } = getWalletDetails();
   if (!address) {
     throw new Error(UI_MESSAGES.walletNotConnected || "Wallet not connected");
   }
 
+  const onExpectedNetwork = await checkCurrentNetwork(provider);
+  if (!onExpectedNetwork) {
+    throw new Error(UI_MESSAGES.wrongNetwork || "Wrong network");
+  }
+
   try {
-    const selfGate = getSelfGate(true);
+    const selfGate = getSelfGate(false);
+    const callData = selfGate.interface.encodeFunctionData(
+      "checkMyVerification"
+    );
+    const rawResult = await selfGate.provider.call({
+      to: SELF_GATE_ADDRESS,
+      data: callData,
+      from: address,
+    });
+
     const [isVerified, lastVerifiedAt, selfChainId, selfProof] =
-      await selfGate.checkMyVerification();
+      selfGate.interface.decodeFunctionResult("checkMyVerification", rawResult);
 
     return {
       address,
